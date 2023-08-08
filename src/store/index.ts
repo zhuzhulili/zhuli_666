e36fcfeb36d903207969442747d6d35254d51f84
zhuzhulili <www.aaaaiii@foxmail.com>
2023-08-08 17:42:51 +0800

    提交

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @LastEditors: zl
+ * @Date: 2023-08-08 14:27:09
+ * @LastEditTime: 2023-08-08 14:27:54
+ * @Email:
+ * @Description:
+ */
+import { createPinia } from "pinia";
+
+export default createPinia();
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/*
+ * @LastEditors: zl
+ * @Date: 2023-08-08 09:56:34
+ * @LastEditTime: 2023-08-08 14:05:23
+ * @Email:
+ * @Description:
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
